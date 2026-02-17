cc8f26f1caeed335c6abbe6d9842a7b77f1cc876
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2026-02-17 15:40:49 +0100

    part2-proxy: first solution half duplex
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- part2-proxy/src/main.rs
+++ part2-proxy/src/main.rs
@@ -1,8 +1,10 @@
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()>{
-
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Proxy listening on 127.0.0.1:8080");
 
@@ -18,8 +20,25 @@ async fn main() -> std::io::Result<()>{
     }
 }
 
-async fn handle_connection(client: TcpStream) -> std::io::Result<()> {
+async fn handle_connection(mut client: TcpStream) -> std::io::Result<()> {
+    // Target : connect to upstream ( server ) at 127.0.0.1:9090
+    // and forward downstream (client ) data to it.
 
+    let mut upstream = TcpStream::connect("127.0.0.1:9090").await?;
+    println!("connected to upstream");
+
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        let bytes_read = client.read(&mut buffer).await?;
+        if bytes_read == 0 {
+            println!("Client disconnected");
+            break;
+        }
+
+        println!("Forwarding {} bytes to upstream", bytes_read);
+        upstream.write_all(&buffer[..bytes_read]).await?;
+    }
 
     Ok(())
 }
