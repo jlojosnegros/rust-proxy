37dacb9a0a1755750b9d6967eaa049e2515b6b98
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2026-02-18 12:32:03 +0100

    part2-proxy: adding logging and tracing
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- part2-proxy/src/main.rs
+++ part2-proxy/src/main.rs
@@ -1,16 +1,22 @@
+mod proxy;
 use std::time::Duration;
 
 use tokio::{
-    io::copy_bidirectional,
+    io::{AsyncWriteExt, copy_bidirectional},
     net::{TcpListener, TcpStream},
     time::timeout,
 };
+use tracing::{debug, error, info, instrument, warn};
 
 const CONNECT_TIMEOUT: Duration = Duration::from_secs(5);
 const IDLE_TIMEOUT: Duration = Duration::from_secs(60);
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Proxy listening on 127.0.0.1:8080");
 
@@ -31,18 +37,27 @@ async fn main() -> std::io::Result<()> {
     }
 }
 
+#[instrument(skip(client), fields(client_addr = %client.peer_addr().unwrap()))]
 async fn handle_connection(mut client: TcpStream) -> std::io::Result<(u64, u64)> {
     // connect to upstream ( server ) at 127.0.0.1:9090
+    debug!("Connecting to upstream");
     let upstream_result = timeout(CONNECT_TIMEOUT, TcpStream::connect("127.0.0.1:9090")).await;
 
     let mut upstream = match upstream_result {
-        Ok(Ok(tcp_stream)) => tcp_stream,
+        Ok(Ok(tcp_stream)) => {
+            info!("Upstream connected");
+            tcp_stream
+        }
         Ok(Err(e)) => {
-            eprintln!("Failed to connecto to upstream: {}", e);
+            let msg = format!("Failed to connecto to upstream: {}", e);
+            eprintln!("{}", msg);
+            let _ = client.write_all(msg.as_bytes()).await;
+            error!(msg);
             return Err(e);
         }
         Err(_) => {
             eprintln!("Upstream connection timeout");
+            warn!("Upstream connection timeout");
             return Err(std::io::Error::new(
                 std::io::ErrorKind::TimedOut,
                 "upstream connection timeout",
@@ -56,10 +71,17 @@ async fn handle_connection(mut client: TcpStream) -> std::io::Result<(u64, u64)>
     let result = timeout(IDLE_TIMEOUT, copy_bidirectional(&mut client, &mut upstream)).await;
 
     match result {
-        Ok(Ok((up, down))) => Ok((up, down)),
-        Ok(Err(e)) => Err(e),
+        Ok(Ok((up, down))) => {
+            info!(bytes_up = up, bytes_down = down, "Transfer complete");
+            Ok((up, down))
+        }
+        Ok(Err(e)) => {
+            warn!(error = %e, "Transfer failed");
+            Err(e)
+        }
         Err(_) => {
             eprintln!("Idle timeout - no data for {:?}", IDLE_TIMEOUT);
+            info!("Idle timeout - no data for {:?}", IDLE_TIMEOUT);
             Err(std::io::Error::new(
                 std::io::ErrorKind::TimedOut,
                 "idle timeout",
